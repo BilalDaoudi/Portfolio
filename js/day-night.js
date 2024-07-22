b9b578efaf001576c96915bfe11915049b041ec6
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-07-22 09:11:42 +0100

    Update day-night.js

--- js/day-night.js
+++ js/day-night.js
@@ -1,4 +1,6 @@
 const dayNight = document.querySelector(".day-night");
+
+
 dayNight.addEventListener("click", () => {
     dayNight.querySelector("i").classList.toggle("fa-sun");
     dayNight.querySelector("i").classList.toggle("fa-moon");
