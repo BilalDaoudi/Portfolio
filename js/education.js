176a9f2ab43cbe40dd6e4a1668d32c7368f9068d
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-07-31 12:46:21 +0100

    Update education.js

--- js/education.js
+++ js/education.js
@@ -3,6 +3,7 @@ var educations = [
     {date : "2020-2021" , titre : "General Economics" , description : "Student at the Multidisciplinary Faculty of Nador, majoring in General Economics." },
     {date : "2019-2020" , titre : "Baccalaureate" , description : "Baccalaureate in Life and Earth Sciences from Taha Hussein High School in Jaadar Zegangan, Nador." },
 ];
+
 function Educations(){
     let rowsducation = document.getElementById("educations");
     let html = "";
@@ -17,4 +18,5 @@ function Educations(){
     </div>`);
     rowsducation.innerHTML = html;
 }
+
 Educations();
