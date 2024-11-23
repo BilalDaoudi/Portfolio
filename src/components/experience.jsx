d7b3146cbcc3429d5da88d362ca116df5a36970a
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-11-23 13:37:59 +0100

    Update experience.jsx

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -5,15 +5,11 @@ const Experience = (props) => {
         <div className="timeline-item">
             <div className="timeline-item-inner outer-shadow">
                 <i className="fas fa-briefcase icon"></i>
-                
                 <span>{date}</span>
                 <h3>{titre}</h3>
                 <h4>{description}</h4>
-                
             </div>
         </div>
-
-        
     );
 };
 
