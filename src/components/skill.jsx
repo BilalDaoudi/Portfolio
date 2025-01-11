4b0a7319f7dd51cb28ebab39e9d4bde91c737201
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-01-11 20:13:31 +0100

    Update skill.jsx

--- src/components/skill.jsx
+++ src/components/skill.jsx
@@ -6,10 +6,19 @@ const Skill = (props) => {
     return (
         <div className="skill-items">
             <p>{titre}</p>
-            <div className="progress inner-shadow">
+            <div className="progress inner-shadow">  
+
+
+                
                 <div className="progress-bar inner-shadow" style={{ width: `calc(${percentage} - 14px)` }}>
+
+
+                    
                     <span>{percentage}</span>
                 </div>
+
+
+                
             </div>
         </div>
     );
