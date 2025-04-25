c39e476595a427e8bbcb2af1275fa8b112f0fc47
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-04-25 17:26:10 +0000

    Update project.jsx

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 const Project = ({ project }) => {
+
+
+
+
+  
   const langue = useSelector((data) => data.langue);
 
   
