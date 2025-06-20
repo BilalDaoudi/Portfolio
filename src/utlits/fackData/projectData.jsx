e46cc98bee7a3221f8e3a323fdb0e07947e7aca9
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-06-20 17:53:57 +0000

    Add files via upload

---
+++ src/utlits/fackData/projectData.jsx
@@ -0,0 +1,46 @@
+import work_1 from "../../assets/images/projects/1.png"
+import work_2 from "../../assets/images/projects/2.png"
+import work_3 from "../../assets/images/projects/3.png"
+import work_4 from "../../assets/images/projects/4.png"
+import work_5 from "../../assets/images/projects/5.png"
+import work_6 from "../../assets/images/projects/6.png"
+
+export const projectsData = [
+    {
+        id: 1,
+        src: work_1,
+        category: "Web application",
+        title: "Mobile Application Design",
+    },
+    {
+        id: 2,
+        src: work_2,
+        category: "WordPress site",
+        title: "Website Makeup Design",
+    },
+    {
+        id: 3,
+        src: work_3,
+        category: "E-commerce site",
+        title: "Brand Identity and Motion Design",
+    },
+    {
+        id: 4,
+        src: work_4,
+        category: "Static site",
+        title: "Mobile Application Design",
+    },
+    {
+        id: 5,
+        src: work_5,
+        category: "Landing page",
+        title: "Brand Identity and Motion Design",
+    },
+    {
+        id: 6,
+        src: work_6,
+        category: "WordPress site",
+        title: "Brand Identity and Motion Design"
+    },
+
+]
